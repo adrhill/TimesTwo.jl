54ad36b88ba99d13b948b5fb4f196f264da652f2
Documenter.jl <documenter@juliadocs.github.io>
2026-05-19 15:40:23 +0000

    build based on 4cfe854

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"category":"section","location":"#TimesTwo","page":"Home","text":"Documentation for TimesTwo.\n\n","title":"TimesTwo"}]
+[{"category":"section","location":"#TimesTwo","page":"Home","text":"Documentation for TimesTwo.\n\n","title":"TimesTwo"},{"category":"method","location":"#TimesTwo.times_two-Tuple{Any}","page":"Home","text":"times_two(x)\n\nMultiplies inputs by two. asdf\n\n\n\n\n\n","title":"TimesTwo.times_two"}]
 }
